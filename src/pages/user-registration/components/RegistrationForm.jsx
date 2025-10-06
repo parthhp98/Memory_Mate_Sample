4d22b6e27f42143e22c2a641003e6cc28b3ff13a
Tazeem <CodingJourney786@gmail.com>
2025-10-06 12:10:19 +0530

    Update RegistrationForm.jsx

--- src/pages/user-registration/components/RegistrationForm.jsx
+++ src/pages/user-registration/components/RegistrationForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import Input from "../../../components/ui/Input";
-import Button from "../../../components/ui/button";
+import Input from "components/ui/input";
+import Button from "components/ui/button";
 import Select from "../../../components/ui/Select";
 import { Checkbox } from "../../../components/ui/Checkbox";
 
